f9042e00656db8e9eab36856e7571729f1363f5a
J-Lindvig <54498188+J-Lindvig@users.noreply.github.com>
2023-02-02 21:49:26 +0100

    Add files via upload

--- custom_components/flagdays_dk/__init__.py
+++ custom_components/flagdays_dk/__init__.py
@@ -1,115 +1,119 @@
 from __future__ import annotations
 
 import logging
-
-from .flagdays_dk import flagdays_dk
 from collections import OrderedDict
 from datetime import datetime
 
-from homeassistant.const import ATTR_FRIENDLY_NAME, ATTR_DATE
+from homeassistant.const import ATTR_DATE, ATTR_FRIENDLY_NAME
+
 from .const import (
-    CONF_ATTRIBUTE_NAMES,
-    CONF_CLIENT,
-    CONF_EXCLUDE,
-    CONF_FLAGDAYS,
-    CONF_INCLUDE,
-    CONF_OFFSET,
-    CONF_PLATFORM,
-    DEFAULT_ATTRIBUTE_NAMES,
-    DEFAULT_DATE_FORMAT,
-    DEFAULT_OFFSET,
-    DOMAIN,
-    KEY_NAME,
-    KEY_PRIORITY,
+	CONF_ATTRIBUTE_NAMES,
+	CONF_CLIENT,
+	CONF_EXCLUDE,
+	CONF_FLAGDAYS,
+	CONF_INCLUDE,
+	CONF_OFFSET,
+	CONF_PLATFORM,
+	DEFAULT_ATTRIBUTE_NAMES,
+	DEFAULT_DATE_FORMAT,
+	DEFAULT_OFFSET,
+	DOMAIN,
+	KEY_NAME,
+	KEY_PRIORITY
 )
+from .flagdays_dk import flagdays_dk
 
 _LOGGER: logging.Logger = logging.getLogger(__package__)
 _LOGGER = logging.getLogger(__name__)
 
 
 async def async_setup(hass, config):
-    # Get the configuration
-    conf = config.get(DOMAIN)
-    # If no config, abort
-    if conf is None:
-        return True
+	# Get the configuration
+	conf = config.get(DOMAIN)
+	# If no config, abort
+	if conf is None:
+		return True
 
-    # Create a instance of the flagdays_dk
-    flagdays = flagdays_dk(
-        include=config[DOMAIN].get(CONF_INCLUDE, []),
-        exclude=config[DOMAIN].get(CONF_EXCLUDE, []),
-    )
+	# Create a instance of the flagdays_dk
+	# Pass the include/exclude list if any, else pass empty list
+	flagdays = flagdays_dk(
+		include=config[DOMAIN].get(CONF_INCLUDE, []),
+		exclude=config[DOMAIN].get(CONF_EXCLUDE, []),
+	)
 
-    # Extract attribute names to search for - make it lowercase
-    attr_names = set([x.lower() for x in config[DOMAIN].get(CONF_ATTRIBUTE_NAMES, [])])
+	# Extract attribute names to search for - make it lowercase
+	attr_names = set([x.lower() for x in config[DOMAIN].get(CONF_ATTRIBUTE_NAMES, [])])
 
-    def flagdayFromSensor(entity):
-        flagdayObj = hass.states.get(customFlagday)
+	# Load a flagday from a sensor
+	def flagdayFromSensor(entity):
+		# Get the sensor
+		flagdayObj = hass.states.get(customFlagday)
 
-        # Create a list of keys from the attribute names which are present in the attributes
-        attr_date_keys = list(attr_names.intersection(set(flagdayObj.attributes)))
+		# Create a list of keys from the attribute names which are present in the attributes
+		attr_date_keys = list(attr_names.intersection(set(flagdayObj.attributes)))
 
-        # find the first key of type datetime
-        attr_date_key = None
-        for date_key in attr_date_keys:
-            if type(flagdayObj.attributes[date_key]) is datetime:
-                attr_date_key = date_key
-                break
+		# find the first key of type datetime
+		attr_date_key = None
+		for date_key in attr_date_keys:
+			if type(flagdayObj.attributes[date_key]) is datetime:
+				attr_date_key = date_key
+				break
 
-        # Did we find a key of datetime type
-        if attr_date_key:
-            return {
-                flagdayObj.attributes[ATTR_FRIENDLY_NAME]: {
-                    ATTR_DATE: flagdayObj.attributes[attr_date_key].strftime(
-                        DEFAULT_DATE_FORMAT
-                    )
-                }
-            }
+		# Did we find a key of datetime type
+		if attr_date_key:
+			return {
+				flagdayObj.attributes[ATTR_FRIENDLY_NAME]: {
+					ATTR_DATE: flagdayObj.attributes[attr_date_key].strftime(
+						DEFAULT_DATE_FORMAT
+					)
+				}
+			}
 
-    def flagdayFromConfig(customFlagday):
-        flagdayData = dict(customFlagday)
-        flagdayData.update({KEY_PRIORITY: priorityCheck(customFlagday)})
-        return {customFlagday.pop(KEY_NAME): flagdayData}
+	# Load a manual flagday
+	def flagdayFromConfig(customFlagday):
+		flagdayData = dict(customFlagday)
+		flagdayData.update({KEY_PRIORITY: priorityCheck(customFlagday)})
+		return {customFlagday.pop(KEY_NAME): flagdayData}
 
-    def priorityCheck(payload, priority=0):
-        return priority if not KEY_PRIORITY in payload else payload[KEY_PRIORITY]
+	def priorityCheck(payload, priority=0):
+		return priority if not KEY_PRIORITY in payload else payload[KEY_PRIORITY]
 
-    # Dict to hold the possible custom flagdays
-    customFlagdays = {}
-    for customFlagday in config[DOMAIN].get(CONF_FLAGDAYS, []):
+	# Dict to hold the possible custom flagdays
+	customFlagdays = {}
+	for customFlagday in config[DOMAIN].get(CONF_FLAGDAYS, []):
 
-        # Sensor or Group of Sensors
-        if type(customFlagday) is str:
-            domain = customFlagday.split(".", 1)[0]
+		# Sensor or Group of Sensors
+		if type(customFlagday) is str:
+			domain = customFlagday.split(".", 1)[0]
 
-            # Sensor
-            if domain == "sensor":
-                customFlagdays.update(flagdayFromSensor(customFlagday))
+			# Sensor
+			if domain == "sensor":
+				customFlagdays.update(flagdayFromSensor(customFlagday))
 
-            # Group of Sensors
-            elif domain == "group":
-                for customFlagday in hass.states.get(customFlagday).attributes[
-                    "entity_id"
-                ]:
-                    if customFlagday.split(".", 1)[0] == "sensor":
-                        customFlagdays.update(flagdayFromSensor(customFlagday))
+			# Group of Sensors
+			elif domain == "group":
+				for customFlagday in hass.states.get(customFlagday).attributes[
+					"entity_id"
+				]:
+					if customFlagday.split(".", 1)[0] == "sensor":
+						customFlagdays.update(flagdayFromSensor(customFlagday))
 
-        # Element from YAML
-        elif type(customFlagday) is OrderedDict:
-            customFlagdays.update(flagdayFromConfig(customFlagday))
+		# Element from YAML
+		elif type(customFlagday) is OrderedDict:
+			customFlagdays.update(flagdayFromConfig(customFlagday))
 
-    # Add the custom flagdays
-    flagdays.add(customFlagdays)
+	# Add the custom flagdays
+	flagdays.add(customFlagdays)
 
-    hass.data[DOMAIN] = {
-        CONF_CLIENT: flagdays,
-        CONF_OFFSET: config[DOMAIN].get(CONF_OFFSET, DEFAULT_OFFSET),
-    }
+	hass.data[DOMAIN] = {
+		CONF_CLIENT: flagdays,
+		CONF_OFFSET: config[DOMAIN].get(CONF_OFFSET, DEFAULT_OFFSET),
+	}
 
-    # Add sensors
-    hass.async_create_task(
-        hass.helpers.discovery.async_load_platform(CONF_PLATFORM, DOMAIN, conf, config)
-    )
+	# Add sensors
+	hass.async_create_task(
+		hass.helpers.discovery.async_load_platform(CONF_PLATFORM, DOMAIN, conf, config)
+	)
 
-    # Initialization was successful.
-    return True
+	# Initialization was successful.
+	return True
--- custom_components/flagdays_dk/sensor.py
+++ custom_components/flagdays_dk/sensor.py
@@ -10,12 +10,12 @@ import pytz
 
 from homeassistant.const import ATTR_ATTRIBUTION, ATTR_DATE, ATTR_DEVICE_CLASS
 from .const import (
-    CONF_CLIENT,
-    CONF_OFFSET,
-    CONF_PLATFORM,
-    CREDITS,
-    DOMAIN,
-    UPDATE_INTERVAL,
+	CONF_CLIENT,
+	CONF_OFFSET,
+	CONF_PLATFORM,
+	CREDITS,
+	DOMAIN,
+	UPDATE_INTERVAL,
 )
 
 ATTR_CONCURRENT = "concurrent_flagdays"
@@ -37,121 +37,120 @@ _LOGGER = logging.getLogger(__name__)
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
 
-    # Define a update function
-    async def async_update_data():
-        # Retrieve the client stored in the hass data stack
-        flagdays = hass.data[DOMAIN][CONF_CLIENT]
-        # Call, and wait for it to finish, the function with the refresh procedure
-        _LOGGER.debug("Updating flagdays...")
-        await hass.async_add_executor_job(flagdays.update)
+	# Define a update function
+	async def async_update_data():
+		# Retrieve the client stored in the hass data stack
+		flagdays = hass.data[DOMAIN][CONF_CLIENT]
+		# Call, and wait for it to finish, the function with the refresh procedure
+		_LOGGER.debug("Updating flagdays...")
+		await hass.async_add_executor_job(flagdays.update)
 
-    # Create a coordinator
-    coordinator = DataUpdateCoordinator(
-        hass,
-        _LOGGER,
-        name=CONF_PLATFORM,
-        update_method=async_update_data,
-        update_interval=timedelta(minutes=UPDATE_INTERVAL),
-    )
+	# Create a coordinator
+	coordinator = DataUpdateCoordinator(
+		hass,
+		_LOGGER,
+		name=CONF_PLATFORM,
+		update_method=async_update_data,
+		update_interval=timedelta(minutes=UPDATE_INTERVAL),
+	)
 
-    # Immediate refresh
-    await coordinator.async_request_refresh()
+	# Immediate refresh
+	await coordinator.async_request_refresh()
 
-    # Add the sensor to Home Assistant
-    async_add_entities(
-        [FlagDaysSensor(hass, coordinator, hass.data[DOMAIN][CONF_CLIENT])]
-    )
+	# Add the sensor to Home Assistant
+	async_add_entities(
+		[FlagDaysSensor(hass, coordinator, hass.data[DOMAIN][CONF_CLIENT])]
+	)
 
 
 class FlagDaysSensor(SensorEntity):
-    def __init__(self, hass, coordinator, flagdays) -> None:
-        self.hass = hass
-        self.coordinator = coordinator
-        self.flagdays = flagdays
-        self.nextFlagday = flagdays.flagdays[0]
-        self.flagUpTime, self.flagDownTime = 0, 0
-        self.entity_id = "sensor.kast_op"
+	def __init__(self, hass, coordinator, flagdays) -> None:
+		self.hass = hass
+		self.coordinator = coordinator
+		self.flagdays = flagdays
+		self.nextFlagday = flagdays.flagdays[0]
+		self.flagUpTime, self.flagDownTime = 0, 0
 
-    @property
-    def name(self):
-        return DOMAIN
+	@property
+	def name(self):
+		return DOMAIN
 
-    @property
-    def icon(self):
-        return "mdi:flag"
+	@property
+	def icon(self):
+		return "mdi:flag"
 
-    @property
-    def state(self):
-        geo = Observer(
-            self.hass.config.latitude,
-            self.hass.config.longitude,
-            self.hass.config.elevation,
-        )
-        s = sun(geo, date=self.nextFlagday.date, tzinfo=tz.gettz("Europe/Copenhagen"))
-        self.flagUpTime = (
-            datetime.strptime("08:00", "%H:%M")
-            if s["sunrise"].hour < 8
-            else s["sunrise"]
-        )
-        self.flagDownTime = s["sunset"]
+	@property
+	def state(self):
+		geo = Observer(
+			self.hass.config.latitude,
+			self.hass.config.longitude,
+			self.hass.config.elevation,
+		)
+		s = sun(geo, date=self.nextFlagday.date, tzinfo=tz.gettz("Europe/Copenhagen"))
+		self.flagUpTime = (
+			datetime.strptime("08:00", "%H:%M")
+			if s["sunrise"].hour < 8
+			else s["sunrise"]
+		)
+		self.flagDownTime = s["sunset"]
 
-        dt_now = datetime.now().astimezone(pytz.timezone("Europe/Copenhagen"))
-        offset = timedelta(minutes=self.hass.data[DOMAIN][CONF_OFFSET])
+		dt_now = datetime.now().astimezone(pytz.timezone("Europe/Copenhagen"))
+		offset = timedelta(minutes=self.hass.data[DOMAIN][CONF_OFFSET])
 
-        if self.flagUpTime > dt_now:
-            return self.flagUpTime - offset
-        elif (
-            type(self.nextFlagday.halfMast) is datetime
-            and self.nextFlagday.halfMast > dt_now
-        ):
-            return self.nextFlagday.halfMast - offset
-        else:
-            return self.flagDownTime - offset
+		if self.flagUpTime > dt_now:
+			return self.flagUpTime - offset
+		elif (
+			type(self.nextFlagday.halfMast) is datetime
+			and self.nextFlagday.halfMast > dt_now
+		):
+			return self.nextFlagday.halfMast - offset
+		else:
+			return self.flagDownTime - offset
 
-    @property
-    def unique_id(self):
-        return "1708981ec9bb4fbfb5d183771a888af0"
+	@property
+	def unique_id(self):
+		return "1708981ec9bb4fbfb5d183771a888af0"
 
-    @property
-    def extra_state_attributes(self):
-        attr = {
-            ATTR_FLAGDAY_NAME: self.nextFlagday.name,
-            ATTR_DAYS: self.flagdays.days,
-            ATTR_YEARS: self.nextFlagday.years,
-            ATTR_FLAG: self.nextFlagday.flag,
-            ATTR_FLAG_UP: self.flagUpTime.strftime("%H:%M"),
-            ATTR_FLAG_DOWN: self.flagDownTime.strftime("%H:%M"),
-            ATTR_HALF_MAST: self.nextFlagday.halfMast,
-            ATTR_CONCURRENT: self.flagdays.getConcurrentFlagdays(self.nextFlagday.date),
-        }
-        attr["future_flagdays"] = []
-        for flagday in self.flagdays.getFutureFlagdays():
-            attr["future_flagdays"].append(
-                {
-                    ATTR_FLAGDAY_NAME: flagday.name,
-                    ATTR_DATE: flagday.getDate("%-d-%-m"),
-                    ATTR_DATE_END: flagday.getDateEnd("%-d-%-m"),
-                }
-            )
-        attr.update({ATTR_DEVICE_CLASS: "timestamp", ATTR_ATTRIBUTION: CREDITS})
-        return attr
+	@property
+	def extra_state_attributes(self):
+		attr = {
+			ATTR_FLAGDAY_NAME: self.nextFlagday.name,
+			ATTR_DAYS: self.flagdays.days,
+			ATTR_YEARS: self.nextFlagday.years,
+			ATTR_FLAG: self.nextFlagday.flag,
+			ATTR_FLAG_UP: self.flagUpTime.strftime("%H:%M"),
+			ATTR_FLAG_DOWN: self.flagDownTime.strftime("%H:%M"),
+			ATTR_HALF_MAST: self.nextFlagday.halfMast,
+			ATTR_CONCURRENT: self.flagdays.getConcurrentFlagdays(self.nextFlagday.date),
+		}
+		attr["future_flagdays"] = []
+		for flagday in self.flagdays.getFutureFlagdays():
+			attr["future_flagdays"].append(
+				{
+					ATTR_FLAGDAY_NAME: flagday.name,
+					ATTR_DATE: flagday.getDate("%-d-%-m"),
+					ATTR_DATE_END: flagday.getDateEnd("%-d-%-m"),
+				}
+			)
+		attr.update({ATTR_DEVICE_CLASS: "timestamp", ATTR_ATTRIBUTION: CREDITS})
+		return attr
 
-    @property
-    def should_poll(self):
-        """No need to poll. Coordinator notifies entity of updates."""
-        return False
+	@property
+	def should_poll(self):
+		"""No need to poll. Coordinator notifies entity of updates."""
+		return False
 
-    @property
-    def available(self):
-        """Return if entity is available."""
-        return self.coordinator.last_update_success
+	@property
+	def available(self):
+		"""Return if entity is available."""
+		return self.coordinator.last_update_success
 
-    async def async_update(self):
-        """Update the entity. Only used by the generic entity update service."""
-        await self.coordinator.async_request_refresh()
+	async def async_update(self):
+		"""Update the entity. Only used by the generic entity update service."""
+		await self.coordinator.async_request_refresh()
 
-    async def async_added_to_hass(self):
-        """When entity is added to hass."""
-        self.async_on_remove(
-            self.coordinator.async_add_listener(self.async_write_ha_state)
-        )
+	async def async_added_to_hass(self):
+		"""When entity is added to hass."""
+		self.async_on_remove(
+			self.coordinator.async_add_listener(self.async_write_ha_state)
+		)
